754456f2c752b3522e2c580c4896ab375f3ffd46
RazorKort <55944415+RazorKort@users.noreply.github.com>
2025-09-15 21:02:41 +0700

    Add files via upload

---
+++ skam_server.py
@@ -0,0 +1,23 @@
+import asyncio
+import websockets
+import os
+
+clients=()
+
+async def handler(websocket, path):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in clients:
+                if client != websocket:
+                    await client.send(message)
+    finally:
+        clients.remove(websocket)
+        
+async def main():
+    port = int(os.environ.get('PORT',10000))
+    websockets.serve(handler,'0.0.0.0',port)
+    await asyncio.Future()
+    
+if __name__ == '__main__':
+    asyncio.run(main())
