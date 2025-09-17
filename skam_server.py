e3b56243b7363faf766c79cfcb3a5da1e1eb7c61
RazorKort <55944415+RazorKort@users.noreply.github.com>
2025-09-17 23:24:30 +0700

    Add files via upload

--- skam_server.py
+++ skam_server.py
@@ -1,16 +1,55 @@
 import os
 import asyncio
-from fastapi import FastAPI, WebSocket
+import uuid
+from fastapi import FastAPI, WebSocket, HTTPException
 import uvicorn
+import asyncpg
+from pydantic import BaseModel
 
 app = FastAPI()
 clients = set()
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
+class UserAuth(BaseModel):
+    name:str
+    uuid:str
+    user_id:int
+    
+
+@app.on_event('startup')
+async def startup():
+    app.state.pool = await asyncpg.create_pool(DATABASE_URL)
+    
+@app.on_event('shutdown')
+async def shutdown():
+    await app.state.pool.close()
 
 @app.get("/", include_in_schema=False)
 @app.head("/", include_in_schema=False)
 async def healthcheck():
     return {"status": "ok"}
+
+@app.post('/auth')
+async def auth(user: UserAuth):
+
+    query = 'SELECT id, name FROM users WHERE uuid = $1'
+    async with app.state.pool.acquire() as conn:
+        row = await conn.fetchrow(query, user.uuid)
+    if row:
+        return {'status': 'ok', 'name': row['name'], 'user_id': row['id']}
+    else:
+        raise HTTPException(status_code = 401, detail = 'User not found')
+    
+
+@app.post('/register')
+async def register(user: UserAuth):
+    if not user.name:
+        raise HTTPException(status_code = 400, detail = 'Name rquired')
+    user_uuid = str(uuid.uuid4())
+    query = 'INSERT INTO users (uuid, name) VALUES ($1, $2) RETURNING id'
+    async with app.state.pool.acquire() as conn:
+        user_id = await conn.fetchval(query, user_uuid, user.name)
+    return {'status': 'ok', 'user_id': user_id, 'uuid': user_uuid}
 
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
