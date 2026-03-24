9a7c56dd297315035777681e4832489f20d6e6d4
Razor_kort <razorkort@gmail.com>
2026-03-24 22:16:36 +0700

    Broke everything

--- skam_server.py
+++ skam_server.py
@@ -81,7 +81,7 @@ async def healthcheck():
 
 @app.post('/ping')
 async def ping():
-    return {'status':'pong'}
+    return {'status':'ok', 'details':'pong'}
 
 @app.post('/auth-request')
 async def auth(user: AuthRequest):
@@ -157,7 +157,7 @@ async def get_friends(user: GetFriends):
     async with app.state.pool.acquire() as conn:
         rows = await conn.fetch(query, user_id)
     if not rows:
-        return {'status':'lonely'}
+        return {'status':'ok', 'details':'lonely'}
     else:
         friends = [dict(row) for row in rows]
         return {'status':'ok', 'friends':friends}
@@ -221,18 +221,18 @@ async def getpublic(user: GetPublic):
     if public_key is not None:
         return {'status': 'ok', 'public_key': public_key}
     else:
-        return {'status': 'error'}
+        return {'status': 'error', 'detail':'not found'}
 
 @app.post('/messages')
 async def msgs(user: LoadMessages):
     user_id = decode_jwt(user.token)
     friend_id = user.target_id
     
-    query = 'SELECT * FROM messages WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $1) ORDER BY created_at ASC'
+    query = 'SELECT * FROM messages WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $1)'
     async with app.state.pool.acquire() as conn:
         rows = await conn.fetch(query, user_id, friend_id)
         if not rows:
-            return {'status':'none', 'message':'сообщений нет'}
+            return {'status':'ok', 'message':'сообщений нет'}
         else: 
             messages = [dict(row) for row in rows]
             return {'status':'ok', 'messages':messages}
@@ -276,18 +276,19 @@ async def websocket_endpoint(ws: WebSocket, token:str):
     try:
         while True:
             msg_data = await ws.receive_json()
-            target_id = msg_data.get('target_id')
+            receiver_id = msg_data.get('receiver_id')
             message = msg_data.get('message')
             name = msg_data.get('name')
             
             query = 'INSERT INTO messages (sender_id, receiver_id, message, name) VALUES ($1, $2, $3, $4)'
             async with app.state.pool.acquire() as conn:
-                await conn.execute(query, user_id, target_id, message, name)
+                await conn.execute(query, user_id, receiver_id, message, name)
                 
 
-            if target_id in clients:
-                await clients[target_id].send_json({'from':user_id, 'message':message, 'name':name})
-    except Exception:
+            if receiver_id in clients:
+                await clients[receiver_id].send_json({'from':user_id, 'message':message, 'name':name})
+    except Exception as e:
+        logger.exception(e)
         pass
     finally:
         clients.pop(user_id, None)
